630786ce0840981d7a8a67aa6b1c48a2da776486
Peter Lodri <silentpocok@gmail.com>
2016-12-09 19:04:13 +0100

    modified MSC __str__

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from csv_reader import readCSV
 
 class BaseData():
     def __init__(self, keys, data):
-        
+        self.keys = keys
+
         for i in range(len(keys)):
             key = keys[i]
             value = data[i]
@@ -12,10 +13,12 @@ class MSC(BaseData):
     
     
     def __str__(self):
-        x = 'subscriber: ' + self.subscriber
-        return str(x)
+        return "dataset: {}, subscriber: {}, TAC: {}, type: {}, timestamp: {}, unix: {}, latitude: {}, longitude: {}".format(
+            self.dataset, self.subscriber, self.TAC, self.type, self.timestamp, self.unix, self.latitude, self.longitude
+        )
 
         
 
 x = readCSV('data/msc_weekly.csv', MSC)
-print(x[1])
+for i in x:
+    print(i.latitude, i.longitude)
