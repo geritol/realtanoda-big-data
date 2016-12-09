979dd42e7abc98a6f4fe6b02839eb4c69c812d55
Tolnai Gergő <gergo.tolnai@gmail.com>
2016-12-09 18:43:40 +0100

    added base data class

--- csv_reader.py
+++ csv_reader.py
@@ -1,16 +1,17 @@
 import csv
 
 
-def readCSV(file):
-	with open(file, "rt", encoding='utf8') as f:
-		out = []
-		reader = csv.reader(f)
-		for row in reader:
-			out.append(row[0].split(';'))
-	return out
+def readCSV(file, className):
+    
+    with open(file, "rt", encoding='utf8') as f:
+        header = []
+        out = []
+        reader = csv.reader(f)
+        for row in reader:
+            if(len(header) == 0):
+                header = row[0].split(';')
+            else:
+                c = className(header, row[0].split(';'))
+                out.append(c)
+    return out
 
-
-
-msc_weekly = readCSV('../msc_weekly.csv')
-print(msc_weekly[0])
-p
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from csv_reader import readCSV
 
-class MSC():
-    
+class BaseData():
     def __init__(self, keys, data):
         
         for i in range(len(keys)):
             key = keys[i]
             value = data[i]
             setattr(self, key, value)
+
+class MSC(BaseData):
+    
+    
     def __str__(self):
         x = 'subscriber: ' + self.subscriber
         return str(x)
