1773db4e5b329049aa2090189aadd058e46c7f1b
Tolnai Gergő <gergo.tolnai@gmail.com>
2016-12-09 23:34:50 +0100

    tower ranges

--- gen_random_pos.py
+++ gen_random_pos.py
@@ -3,48 +3,6 @@ import random
 import pylab as plt
 
 pi = math.pi
-#
-# tower_x = 46.971740
-# tower_y = 21.087803
-tower_x = 47.513853
-tower_y = 19.0542355
-r = 35/111.3
-
-
-
-# positions = []
-#
-# x = []
-# y = []
-#
-# for i in range(1000):
-# 	q = random.random() * (pi * 2)
-# 	r = random.random() ** 2
-# 	x.append((radius * r) * math.cos(q))
-# 	y.append((radius * r) * math.sin(q))
-#
-# x0 = np.median(x)
-# y0 = np.median(y)
-# r = np.sqrt((x - x0)**2 + (y - y0)**2)
-# r0 = np.percentile(r, 100)
-#
-#
-
-
-
-
-x = []
-y = []
-
-# for i in range(1000):
-# 	t = 2 * pi * random.random()
-# 	u = random.random() + random.random()
-# 	r = 2 - u if u > 1 else u
-# 	x.append(r*math.cos(t))
-# 	y.append(r*math.sin(t))
-#
-# print(x)
-# print(y)
 
 
 def randomCoords(r, x0, y0):
@@ -61,16 +19,20 @@ def randomCoords(r, x0, y0):
 	_x = x / math.cos(y0)
 
 	return [_x + x0, y + y0]
-
-xs = []
-ys = []
-
-for i in range(1000):
-	coord = randomCoords(r, tower_x, tower_y)
-	xs.append(coord[0])
-	ys.append(coord[1])
-
-plt.plot(xs, ys, '.')
-circle = plt.Circle((tower_x, tower_y), r, color='r', fill=False)
-
-plt.show()
+ 
+#tower_x = 47.513853
+#tower_y = 19.0542355
+#r = 35/111.3
+#
+#xs = []
+#ys = []
+#
+#for i in range(1000):
+#	coord = randomCoords(r, tower_x, tower_y)
+#	xs.append(coord[0])
+#	ys.append(coord[1])
+#
+#plt.plot(xs, ys, '.')
+#circle = plt.Circle((tower_x, tower_y), r, color='r', fill=False)
+#
+#plt.show()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pylab as plt
 
 from csv_reader import readCSV
 from gen_random_pos import randomCoords
+from approximate_tower_ranges import gen
 
 class Towers():
     def __init__(self):
@@ -51,8 +52,9 @@ class Towers():
 
             xs.append(coords[0])
             ys.append(coords[1])
-            
-        plt.plot(xs, ys, '.')
+       
+        gen(5000)
+        plt.plot(ys, xs, '.')
                 
 
 
